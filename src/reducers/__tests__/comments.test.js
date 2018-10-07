449c60fa7c43b61b178e21127f6d2edbfe5cc016
isaac <isaac.guan@rxpservices.com>
2018-10-07 22:49:30 +1100

    test unknown type

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -13,3 +13,8 @@ it('handles actions of type SAVE_COMMENT', () => {
 	console.log(newState);
 	expect(newState).toEqual(['new comment']);
 });
+
+it('handles action with unknown type', () => {
+	const newState = commentsReducer([], { type: 'LDASD' });
+	expect(newState).toEqual([]);
+});
